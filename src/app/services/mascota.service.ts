66fb6c596b561a0a75f199d136c462cade866c8e
Josue2407 <54449197+Josue2407@users.noreply.github.com>
2020-11-24 10:36:05 -0600

    Actualizando Services

--- src/app/services/mascota.service.ts
+++ src/app/services/mascota.service.ts
@@ -1,33 +1,42 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import { Mascota } from '../models/mascota';
 
 @Injectable({
-  providedIn: 'root'
-})
+  providedIn: 'root' 
+}) 
 export class MascotaService {
 mascotaList: AngularFireList<any>;
+selectedMascota: Mascota;
   constructor(private firebase: AngularFireDatabase) { }
 
   getdatos() { 
     return this.mascotaList = this.firebase.list('mascotas');
    }
-   insertdatos(nombre_mascota:string,raza:string,color:string,especie:string,fecha_nacimiento:String,nombre_propietario:String,telefono:string,direccion:string,correo:String,password:String) {
+   insertdatos(nombre_mascota:string,raza:string,color:string,especie:string,fecha_nacimiento:String,nombre_propietario:String) {
     // agregar un dato al final de la lista
     //this.mascotaList = this.firebase.list('mascotas');
     this.mascotaList.push({
-      Nombre_mascota: nombre_mascota,
+      Nombre_mascota: nombre_mascota, 
       Raza: raza,
       Color: color,
       Especie:especie,
       Fecha_nacimiento: fecha_nacimiento,
       Nombre_propietario: nombre_propietario,
-      Telefono: telefono,
-      Direccion: direccion,
-      Correo: correo,
-      Password:password
        });
       }
 
+      updateMascota($key:string,nombre,propietario:string,especie:string,raza:string,color:string,fecha:string) {
+        // Utilizando el metodo update de firebase , se envia clave y los parametros que va actualizar 
+        this.mascotaList.update($key, {
+          Nombre_mascota: nombre, 
+          Raza: raza,
+          Color: color,
+          Especie:especie,
+          Fecha_nacimiento: fecha,
+          Nombre_propietario: propietario,
+        });
+       }
       deleteMascota($key: string) {
         this.mascotaList.remove($key);
       }
